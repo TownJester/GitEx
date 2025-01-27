8c7847c0113ae832c279e95d5db47a3571339da9
Madalyn <madalynshea@icloud.com>
2025-01-27 13:58:59 -0700

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,6 +6,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World");
+        Debug.Log("Change to code. Hooray");
     }
 
     // Update is called once per frame
